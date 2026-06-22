6f60bae362a1050852d484b9b18efae7df1ce8b5
sDash151 <souravdilu78090@gmail.com>
2026-06-23 02:53:21 +0530

    app refresh logging out problem 4

--- client/vite.config.js
+++ client/vite.config.js
@@ -44,6 +44,7 @@ export default defineConfig({
       workbox: {
         maximumFileSizeToCacheInBytes: 5000000,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        navigateFallbackDenylist: [/^\/api/],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
